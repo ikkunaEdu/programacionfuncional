70e9e42ee1adf2b7c0c18b6125f1c708fb22c2f6
Diego <sigma.octantis@gmai.com>
2018-04-12 19:45:40 -0300

    mid class

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@ var myapp = function() {
     this.mylist = document.getElementById('mylist');
     this.productList = document.getElementById('productList');
 
-    
-
     this.products = [
         {
             name: 'T-Shirt',
-            description: 'Azul',
+            descripcion: 'Azul',
+            image: 'assets/Teeshirt_femme.svg',
             quantity: 13,
             price: '$1.23'
+        },
+        {
+            name: 'Zapato',
+            descripcion: 'Masculino',
+            image:'assets/shoes.svg',
+            quantity: 5,
+            price: '$20000'
+        },
+        {
+            name: 'Pantalon',
+            descripcion: 'Masculino',
+            image: 'assets/trousers.svg',
+            quantity: 7,
+            price: '$25000'
         }
     ]
 
@@ -19,13 +32,17 @@ var myapp = function() {
         card.className = 'demo-card-square mdl-card mdl-shadow--2dp';
         var title = document.createElement('div');
         title.className = 'mdl-card__title mdl-card--expand';
+        title.style = `            color: #fff;
+            background:
+                url('`+productElement.image+`') bottom right 15% no-repeat #46B6AC;
+            }`;
         var h2 = document.createElement('h2');
         h2.className = 'mdl-card__title-text';
-        h2.innerHTML = 'My Title';
+        h2.innerHTML = productElement.name;
         title.appendChild(h2);
         var description = document.createElement('div');
         description.className = 'mdl-card__supporting-text';
-        description.innerHTML = 'My description';
+        description.innerHTML =productElement.descripcion;
         var addbutton = document.createElement('div');
         addbutton.className = 'mdl-card__actions mdl-card--border';
         var a = document.createElement('a');
@@ -65,5 +82,6 @@ var myapp = function() {
 window.onload = function () {
     this.app = new myapp();
     console.log("Productos:",app.products)
-    app.addProduct(app.productList, 'new element');
+    // app.addProduct(app.productList, {name:'Zapatos',descripcion:'De vestir'});
+    app.addProduct(app.productList, app.products[0]);
 }
